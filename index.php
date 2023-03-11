04601e4daa1dec940fc3f2848ed87ed75480bd72
ToledoPD <127598957+ToledoPD@users.noreply.github.com>
2023-03-11 08:29:49 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,21 @@
+<html>
+<body>
+  <h1> Advinhe o numero que estou pensando?<\h1>
+  <form method="post">
+    <input type="number" name="numero_tentativa"></input>
+    <button type="submit">Tentar</button>	
+  </form>
+  <?php
+    $numero_certo = rand(0,10);
+	$numero_escolhido = $_POST["numero_tentativa"];
+	if ($numero_certo == $numero_escolhido) 
+		{
+			echo ("Parabens você acertou o numero!");
+		}
+	else 
+	{
+		echo ("Errou");
+	}
+  ?>		
+</body>
+</html>
